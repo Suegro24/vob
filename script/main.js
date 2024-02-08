bea4a9aacdaa1e4d4d5d34950965864e62634222
Suegro24 <marciu199@gmail.com>
2024-02-08 22:06:19 +0100

    Initial load

---
+++ script/main.js
@@ -0,0 +1,25 @@
+const language = document.querySelector('#language');
+const hamburger = document.querySelector('#hamburger');
+
+const changeLanguage = function() {
+    const lang = this.value;
+    
+    switch(lang) {
+        case "pl": {
+            window.location = '../index.html';
+            break;
+        }
+        case "eng": {
+            window.location = '/eng/index.html';
+            break;
+        }
+    }
+}
+
+const switchMenu = function() {
+    const menu = document.querySelector('#menu');
+    menu.style.display = 'flex';
+}
+
+language.addEventListener('change', changeLanguage, false);
+hamburger.addEventListener('click', switchMenu, false);
